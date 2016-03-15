6fa99899af2b2566854954fd058bd1e90ffe49cb
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-03-15 00:04:20 -0400

    move integration stepping

--- scf/setup_package.py
+++ scf/setup_package.py
@@ -14,6 +14,7 @@ def get_extensions():
     cfg['sources'].append('scf/src/indexx.c')
     cfg['sources'].append('scf/src/helpers.c')
     cfg['sources'].append('scf/src/nrutil.c')
+    cfg['sources'].append('scf/src/leapfrog.c')
     cfg['libraries'] = ['gsl', 'gslcblas']
     cfg['extra_compile_args'] = ['--std=gnu99']
 
---
+++ scf/src/leapfrog.c
@@ -0,0 +1,25 @@
+#include "scf.h"
+
+void step_pos(Config config, Bodies b, double dt,
+              double *tnow, double *tpos) {
+    int k;
+    for (k=0; k<config.n_bodies; k++) {
+        b.x[k] = b.x[k] + dt*b.vx[k];
+        b.y[k] = b.y[k] + dt*b.vy[k];
+        b.z[k] = b.z[k] + dt*b.vz[k];
+    }
+    *tpos = *tpos + dt;
+    *tnow = *tpos;
+}
+
+void step_vel(Config config, Bodies b, double dt,
+             double *tnow, double *tvel) {
+    int k;
+    for (k=0; k<config.n_bodies; k++) {
+        b.vx[k] = b.vx[k] + dt*b.ax[k];
+        b.vy[k] = b.vy[k] + dt*b.ay[k];
+        b.vz[k] = b.vz[k] + dt*b.az[k];
+    }
+    *tvel = *tvel + dt;
+    *tnow = *tvel;
+}
