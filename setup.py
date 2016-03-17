15e194cc9ed3256c76934f4f2cd9fdb0ca9682d4
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-03-17 10:47:39 -0400

    ignore scripts directory for now

--- setup.py
+++ setup.py
@@ -66,8 +66,10 @@ generate_version_py(PACKAGENAME, VERSION, RELEASE,
                     get_debug_option(PACKAGENAME))
 
 # Treat everything in scripts except README.rst as a script to be installed
-scripts = [fname for fname in glob.glob(os.path.join('scripts', '*'))
-           if os.path.basename(fname) != 'README.rst']
+# APW HACK: don't install scripts
+scripts = []
+# scripts = [fname for fname in glob.glob(os.path.join('scripts', '*'))
+#           if os.path.basename(fname) != 'README.rst']
 
 
 # Get configuration information from all of the various subpackages.
