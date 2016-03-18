c981b2786b8355291594a84ed35a8fa6a2e8d4c8
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-03-18 14:27:27 -0400

    include gary path

--- scf/setup_package.py
+++ scf/setup_package.py
@@ -1,13 +1,20 @@
 # Licensed under a 3-clause BSD style license - see PYFITS.rst
 from __future__ import absolute_import
 
+import os
 from distutils.core import Extension
 from astropy_helpers import setup_helpers
 
 def get_extensions():
 
+    # Get gary path
+    import gary
+    gary_base_path = os.path.split(gary.__file__)[0]
+    gary_incl_path = os.path.abspath(os.path.join(gary_base_path, '..'))
+
     cfg = setup_helpers.DistutilsExtensionArgs()
     cfg['include_dirs'].append('numpy')
+    cfg['include_dirs'].append(gary_incl_path)
     cfg['include_dirs'].append('scf/src')
     cfg['sources'].append('scf/scf.pyx')
     cfg['sources'].append('scf/src/scf.c')
