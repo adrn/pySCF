1ff27bfdcc4e4656108c0c335f9b70d81c696d38
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-04-27 13:06:35 -0400

    starting to test C code

--- scf/tests/helpers.pyx
+++ scf/tests/helpers.pyx
@@ -34,6 +34,10 @@ cdef extern from "src/cpotential.h":
     ctypedef struct CPotential:
         pass
 
+cdef extern from "src/helpers.h":
+    int getIndex2D(int row, int col, int ncol)
+    int getIndex3D(int row, int col, int dep, int ncol, int ndep)
+
 cdef extern from "src/scf.h":
     ctypedef struct Config:
         int n_steps
@@ -103,6 +107,7 @@ cdef extern from "src/scf.h":
         int *pot_idx
 
     void accp_firstc(Config config, Placeholders p) nogil
+    void accp_bfe(Config config, Bodies b, Placeholders p, int *firstc) nogil
 
     void acc_pot(Config config, Bodies b, Placeholders p, COMFrame *f,
                  CPotential *pot, double extern_strength, double *tnow,
@@ -123,13 +128,16 @@ cdef extern from "src/scf.h":
     void check_progenitor(int iter, Config config, Bodies b, Placeholders p,
                           COMFrame *f, CPotential *pot, double *tnow) nogil
 
-cpdef _test_accp_firstc():
+cpdef wrap_getIndex2D(int row, int col, int ncol):
+    return getIndex2D(row, col, ncol)
+
+cpdef wrap_getIndex3D(int row, int col, int dep, int ncol, int ndep):
+    return getIndex3D(row, col, dep, ncol, ndep)
+
+cpdef _test_accp_firstc(int nmax, int lmax):
     cdef:
         Config config
         Placeholders p
-
-        int nmax = 6
-        int lmax = 4
 
         double[::1] dblfact = np.zeros(lmax+1)
         double[::1] twoalpha = np.zeros(lmax+1)
@@ -159,3 +167,111 @@ cpdef _test_accp_firstc():
     p.c2 = &c2[0,0]
     p.c3 = &c3[0]
 
+    accp_firstc(config, p)
+
+    return {
+        'dblfact': dblfact,
+        'twoalpha': twoalpha,
+        'anltilde': anltilde,
+        'coeflm': coeflm,
+        'c1': c1,
+        'c2': c2,
+        'c3': c3
+    }
+
+cpdef _test_accp_bfe(bodies):
+    cdef:
+        Config config
+        Placeholders p
+        Bodies b
+
+        int nmax = 6
+        int lmax = 4
+
+        # Read in the phase-space positions of the N bodies
+        int N = bodies.shape[0]
+        double[::1] x = np.ascontiguousarray(bodies.pos.value[0][:])
+        double[::1] y = np.ascontiguousarray(bodies.pos.value[1][:])
+        double[::1] z = np.ascontiguousarray(bodies.pos.value[2][:])
+        double[::1] vx = np.ascontiguousarray(bodies.vel.value[0][:])
+        double[::1] vy = np.ascontiguousarray(bodies.vel.value[1][:])
+        double[::1] vz = np.ascontiguousarray(bodies.vel.value[2][:])
+        double[::1] mass = np.ones(N) / N
+        int[::1] ibound = np.ones(N, dtype=np.int32)
+        double[::1] tub = np.zeros(N)
+        double[::1] ax = np.zeros(N)
+        double[::1] ay = np.zeros(N)
+        double[::1] az = np.zeros(N)
+        double[::1] pot = np.zeros(N) # (internal) potential energy
+        double[::1] kin = np.zeros(N) # kinetic energy
+
+        double[::1] dblfact = np.zeros(lmax+1)
+        double[::1] twoalpha = np.zeros(lmax+1)
+        double[:,::1] anltilde = np.zeros((nmax+1,lmax+1))
+        double[:,::1] coeflm = np.zeros((lmax+1,lmax+1))
+        double[:,::1] c1 = np.zeros((nmax+1,lmax+1))
+        double[:,::1] c2 = np.zeros((nmax+1,lmax+1))
+        double[::1] c3 = np.zeros(nmax+1)
+        double[:,::1] plm = np.zeros((lmax+1,lmax+1))
+        double[:,::1] dplm = np.zeros((lmax+1,lmax+1))
+        double[:,::1] ultrasp = np.zeros((nmax+1,lmax+1))
+        double[:,::1] ultraspt = np.zeros((nmax+1,lmax+1))
+        double[:,::1] ultrasp1 = np.zeros((nmax+1,lmax+1))
+        double[:,:,::1] sinsum = np.zeros((nmax+1,lmax+1,lmax+1))
+        double[:,:,::1] cossum = np.zeros((nmax+1,lmax+1,lmax+1))
+
+        int lmin = 0
+        int lskip = 0
+
+        int firstc = 1
+
+    # minimum set of parameters to initialize
+    config.n_bodies = N
+    config.nmax = nmax
+    config.lmax = lmax
+    config.zeroodd = 0
+    config.zeroeven = 0
+
+    # The N bodies
+    b.x = &x[0]
+    b.y = &y[0]
+    b.z = &z[0]
+    b.vx = &vx[0]
+    b.vy = &vy[0]
+    b.vz = &vz[0]
+    b.ax = &ax[0]
+    b.ay = &ay[0]
+    b.az = &az[0]
+    b.pot = &pot[0]
+    b.kin = &kin[0]
+    b.mass = &mass[0]
+    b.ibound = &ibound[0]
+    b.tub = &tub[0]
+
+    # Pointers to a bunch of placeholder arrays
+    p.lmin = &lmin
+    p.lskip = &lskip
+    p.dblfact = &dblfact[0]
+    p.twoalpha = &twoalpha[0]
+    p.anltilde = &anltilde[0,0]
+    p.coeflm = &coeflm[0,0]
+    p.plm = &plm[0,0]
+    p.dplm = &dplm[0,0]
+    p.ultrasp = &ultrasp[0,0]
+    p.ultraspt = &ultraspt[0,0]
+    p.ultrasp1 = &ultrasp1[0,0]
+    p.sinsum = &sinsum[0,0,0]
+    p.cossum = &cossum[0,0,0]
+    p.c1 = &c1[0,0]
+    p.c2 = &c2[0,0]
+    p.c3 = &c3[0]
+
+    accp_bfe(config, b, p, &firstc)
+
+    return {
+        'sinsum': sinsum,
+        'cossum': cossum,
+        'plm': plm,
+        'ultrasp': ultrasp,
+        'ultraspt': ultraspt,
+    }
--- scf/tests/test_scf_c.py
+++ scf/tests/test_scf_c.py
@@ -8,11 +8,72 @@ import sys
 
 # Third-party
 from astropy import log as logger
+from astropy.utils.data import get_pkg_data_filename
+from astropy.io import ascii
 import astropy.units as u
 import numpy as np
+import six
+
+import gary.dynamics as gd
 
 # Project
-from .helpers import test_accp_firstc
+from .helpers import _test_accp_firstc, _test_accp_bfe
 
 def test_accp_firstc():
-    test_accp_firstc()
+    split_pattr = "# -----------------------------------------------------------------------------"
+
+    # don't change these
+    nmax = 6
+    lmax = 4
+
+    # from Cython implementation
+    d = _test_accp_firstc(nmax, lmax)
+
+    # load "truth" from Fortran
+    filename = get_pkg_data_filename('data/accp_firstc.txt')
+
+    with open(filename,'r') as f:
+        chunks = f.read().split(split_pattr)
+
+    for chunk in chunks:
+        tbl = ascii.read(chunk)
+        name = tbl.colnames[-1]
+
+        if len(tbl.colnames) == 2:
+            for i,val in tbl:
+                print(name, i)
+                assert np.allclose(d[name][i], val)
+
+        elif len(tbl.colnames) == 3:
+            for i,j,val in tbl:
+                print(name, i, j)
+                assert np.allclose(d[name][i,j], val)
+
+def test_accp_bfe():
+    skip = 1
+    names = ['m','x','y','z','vx','vy','vz']
+    bodies_filename = get_pkg_data_filename('data/SCFBI')
+    bodies = np.genfromtxt(bodies_filename, dtype=None, names=names,
+                           skip_header=skip)
+
+    b = gd.CartesianPhaseSpacePosition(pos=bodies[['x','y','z']].view(np.float64).reshape(-1,3).T,
+                                       vel=bodies[['vx','vy','vz']].view(np.float64).reshape(-1,3).T)
+
+    d = _test_accp_bfe(b)
+
+    # plm - load "truth" from Fortran
+    f77_tbl = ascii.read(get_pkg_data_filename('data/plm.txt'))
+    for l,m,plm in f77_tbl:
+        assert np.allclose(plm,d['plm'][l,m])
+
+    # sinsum and cossum - load "truth" from Fortran
+    f77_tbl = ascii.read(get_pkg_data_filename('data/sincossum.txt'))
+    for n,l,m,sinsum,cossum in f77_tbl:
+        assert np.allclose(sinsum, d['sinsum'][n,l,m])
+        assert np.allclose(cossum, d['cossum'][n,l,m])
+
+    # ultrasp and ultraspt - load "truth" from Fortran
+    f77_tbl = ascii.read(get_pkg_data_filename('data/ultrasp.txt'))
+    for n,l,ultrasp,ultraspt in f77_tbl:
+        assert np.allclose(ultrasp, d['ultrasp'][n,l])
+        assert np.allclose(ultraspt, d['ultraspt'][n,l])
