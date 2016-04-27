4662351d7bae740257bf25056aa4b8d6b13a4a59
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-04-27 13:06:16 -0400

    build tests

--- scf/setup_package.py
+++ scf/setup_package.py
@@ -1,4 +1,3 @@
-# Licensed under a 3-clause BSD style license - see PYFITS.rst
 from __future__ import absolute_import
 
 import os
@@ -12,20 +11,25 @@ def get_extensions():
     gary_base_path = os.path.split(gary.__file__)[0]
     gary_path = os.path.join(gary_base_path, 'potential')
 
+    extensions = []
+
     cfg = setup_helpers.DistutilsExtensionArgs()
     cfg['include_dirs'].append('numpy')
     cfg['include_dirs'].append(gary_path)
-    cfg['include_dirs'].append('scf/src')
+    # cfg['include_dirs'].append('scf/src')
     cfg['sources'].append('scf/scf.pyx')
     cfg['sources'].append('scf/src/scf.c')
-    cfg['sources'].append('scf/src/indexx.c')
     cfg['sources'].append('scf/src/helpers.c')
-    cfg['sources'].append('scf/src/nrutil.c')
     cfg['sources'].append('scf/src/leapfrog.c')
+
+    # need to include this for some reason
+    cfg['sources'].append(os.path.join(gary_path, 'src', 'cpotential.c'))
+
     cfg['libraries'] = ['gsl', 'gslcblas']
     cfg['extra_compile_args'] = ['--std=gnu99']
+    extensions.append(Extension('scf.scf', **cfg))
 
-    return [Extension('scf.scf', **cfg)]
+    return extensions
 
 def get_package_data():
-    return {'scf': ['src/*.h']}
+    return {'scf': ['src/*.h', 'tests/data/*']}
--- scf/tests/setup_package.py
+++ scf/tests/setup_package.py
@@ -1,3 +1,31 @@
+from __future__ import absolute_import
+
+import os
+from distutils.core import Extension
+from astropy_helpers import setup_helpers
+
 def get_package_data():
     return {
         _ASTROPY_PACKAGE_NAME_ + '.tests': ['coveragerc']}
+
+def get_extensions():
+
+    # Get gary path
+    import gary
+    gary_base_path = os.path.split(gary.__file__)[0]
+    gary_path = os.path.join(gary_base_path, 'potential')
+
+    extensions = []
+
+    cfg = setup_helpers.DistutilsExtensionArgs()
+    cfg['include_dirs'].append('numpy')
+    cfg['include_dirs'].append(gary_path)
+    cfg['include_dirs'].append('scf')
+    cfg['sources'].append('scf/tests/helpers.pyx')
+    cfg['sources'].append('scf/src/helpers.c')
+    cfg['sources'].append('scf/src/scf.c')
+    cfg['libraries'] = ['gsl', 'gslcblas']
+    cfg['extra_compile_args'] = ['--std=gnu99']
+    extensions.append(Extension('scf.tests.helpers', **cfg))
+
+    return extensions
