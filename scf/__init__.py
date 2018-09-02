b6fd797df9562efb7e175a92cc2242259096d857
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-09-02 13:14:08 -0600

    update package template

--- scf/__init__.py
+++ scf/__init__.py
@@ -10,6 +10,21 @@ This is an Astropy affiliated package.
 from ._astropy_init import *
 # ----------------------------------------------------------------------------
 
+# Enforce Python version check during package import.
+# This is the same check as the one at the top of setup.py
+import sys
+
+__minimum_python_version__ = "3.5"
+
+class UnsupportedPythonError(Exception):
+    pass
+
+
+minversion = tuple((int(val) for val in __minimum_python_version__.split('.')))
+if sys.version_info < minversion:
+    raise UnsupportedPythonError("packagename does not support Python < "
+                                 "{}".format(__minimum_python_version__))
+
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
     # from example_mod import *
